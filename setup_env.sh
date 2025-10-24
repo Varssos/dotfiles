904a41182aa71da2661e2f99443ca5aef5ce45e6
Varssos <sebastianwach222@gmail.com>
2025-10-24 21:19:27 +0200

    Update bashrc scripts

--- setup_env.sh
+++ setup_env.sh
@@ -17,7 +17,9 @@ if [ $IS_MY_BASHRC_SOURCED -lt 1 ]; then
 else
     IS_MY_BASHRC_COMMENTED=$(grep -E '^source.*.my_bashrc' ~/.bashrc | wc -l)
     if [ $IS_MY_BASHRC_COMMENTED -lt 1 ]; then
-        echo "WARNING! source  .my_bashrc is commented in ~/.bashrc."
+        echo "WARNING! source  .my_bashrc is commented in ~/.bashrc. Uncommenting it..."
+        ESCAPED_PWD=$(echo "$PWD" | sed 's/\//\\\//g')
+        sed -i "s/^#[ ]*source.*my_bashrc/source ${ESCAPED_PWD}\/bashrc\/.my_bashrc/" ~/.bashrc
     else
         echo "source .my_bashrc exist in ~/.bashrc. No need to append."
     fi
