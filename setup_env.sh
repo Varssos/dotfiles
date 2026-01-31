d3b96e51ded797d2b486b71c2dcc0cfa1bdc750a
Varssos <sebastianwach222@gmail.com>
2026-01-31 23:52:55 +0100

    Organise setup_env.sh and add installing fonts to tmux conf

--- setup_env.sh
+++ setup_env.sh
@@ -4,51 +4,80 @@ sudo apt update
 sudo apt install stow -y
 
 # ------Setup bashrc------
-BASHRC="$HOME/.bashrc"
-SOURCE_LINE="source ~/.my_bashrc"
+function setup_bashrc(){
+    BASHRC="$HOME/.bashrc"
+    SOURCE_LINE="source ~/.my_bashrc"
 
-IS_PRESENT=$(grep -E '^\s*#?\s*source\s+~/.my_bashrc' "$BASHRC" | wc -l)
+    IS_PRESENT=$(grep -E '^\s*#?\s*source\s+~/.my_bashrc' "$BASHRC" | wc -l)
 
-if [ "$IS_PRESENT" -lt 1 ]; then
-    echo "source ~/.my_bashrc is not included in ~/.bashrc. Appending at the bottom"
-    echo -e "\n$SOURCE_LINE" >> "$BASHRC"
-else
-    # Check if it's commented out
-    IS_COMMENTED=$(grep -E '^\s*#\s*source\s+~/.my_bashrc' "$BASHRC" | wc -l)
-
-    if [ "$IS_COMMENTED" -gt 0 ]; then
-        echo "WARNING! source ~/.my_bashrc is commented in ~/.bashrc. Uncommenting it..."
-        sed -i 's/^\s*#\s*source\s\+~\/.my_bashrc/source ~\/.my_bashrc/' "$BASHRC"
+    if [ "$IS_PRESENT" -lt 1 ]; then
+        echo "source ~/.my_bashrc is not included in ~/.bashrc. Appending at the bottom"
+        echo -e "\n$SOURCE_LINE" >> "$BASHRC"
     else
-        echo "source ~/.my_bashrc exists in ~/.bashrc. No need to append."
-    fi
-fi
+        # Check if it's commented out
+        IS_COMMENTED=$(grep -E '^\s*#\s*source\s+~/.my_bashrc' "$BASHRC" | wc -l)
 
-stow bashrc
-echo "Bashrc configuration applied."
+        if [ "$IS_COMMENTED" -gt 0 ]; then
+            echo "WARNING! source ~/.my_bashrc is commented in ~/.bashrc. Uncommenting it..."
+            sed -i 's/^\s*#\s*source\s\+~\/.my_bashrc/source ~\/.my_bashrc/' "$BASHRC"
+        else
+            echo "source ~/.my_bashrc exists in ~/.bashrc. No need to append."
+        fi
+    fi
+
+    stow bashrc
+    echo "Bashrc configuration applied."
+}
+
 
 
 # ------Setup tmux------
-sudo apt install tmux git xclip -y # or xsel for tmux-yank
 
-## Install tmux plugin manager (tpm)
-TPM_DIR="$HOME/.tmux/plugins/tpm"
+function install_nerd_fonts(){
+# Based on https://firstan.org/en/software/tmux-linux-mint/
+    TMP_MESLO_DIR="/tmp/Meslo"
 
-if [ ! -d "$TPM_DIR/.git" ]; then
-    echo "Installing tmux plugin manager (tpm)…"
-    git clone https://github.com/tmux-plugins/tpm.git "$TPM_DIR"
-else
-    echo "tpm already installed, skipping"
-fi
+    wget -q -O /tmp/Meslo.zip \
+    https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/Meslo.zip
 
-## Apply tmux configuration
-stow tmux
+    rm -rf "$TMP_MESLO_DIR"
+    mkdir -p "$TMP_MESLO_DIR"
 
-## Install tmux plugins
-~/.tmux/plugins/tpm/scripts/source_plugins.sh
-~/.tmux/plugins/tpm/scripts/install_plugins.sh
+    unzip -q /tmp/Meslo.zip -d "$TMP_MESLO_DIR"
+    rm /tmp/Meslo.zip
 
-## Source the tmux configuration
-tmux source ~/.config/tmux/tmux.conf
+    sudo mv "$TMP_MESLO_DIR" /usr/share/fonts/opentype/Meslo
+    fc-cache -f -v
+}
+function setup_tmux(){
+    sudo apt install tmux git xclip -y # or xsel for tmux-yank
 
-echo "Tmux configuration applied."
+    ## Installl nerd fonts
+    install_nerd_fonts
+
+    ## Install tmux plugin manager (tpm)
+    TPM_DIR="$HOME/.tmux/plugins/tpm"
+
+    if [ ! -d "$TPM_DIR/.git" ]; then
+        echo "Installing tmux plugin manager (tpm)…"
+        git clone https://github.com/tmux-plugins/tpm.git "$TPM_DIR"
+    else
+        echo "tpm already installed, skipping"
+    fi
+
+    ## Apply tmux configuration
+    stow tmux
+
+    ## Install tmux plugins
+    ~/.tmux/plugins/tpm/scripts/source_plugins.sh
+    ~/.tmux/plugins/tpm/scripts/install_plugins.sh
+
+    ## Source the tmux configuration
+    tmux source ~/.config/tmux/tmux.conf
+
+    echo "Tmux configuration applied."
+}
+
+# Execute setup functions
+setup_bashrc
+setup_tmux
